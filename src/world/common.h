ef9f78c8d66a51ff89dee69a3bc4c98618c26e6f
Giulio Paci <giuliopaci@gmail.com>
2016-03-03 17:13:42 +0100

    Added world/ prefix to included headers in common.h.

--- src/world/common.h
+++ src/world/common.h
@@ -5,8 +5,8 @@
 #ifndef WORLD_COMMON_H_
 #define WORLD_COMMON_H_
 
-#include "fft.h"
-#include "macrodefinitions.h"
+#include "world/fft.h"
+#include "world/macrodefinitions.h"
 
 WORLD_BEGIN_C_DECLS
 
