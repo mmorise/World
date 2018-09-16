2a1f7b2c773acba65c4b8ba62fcf6e86266772fd
abylouw <22814737+abylouw@users.noreply.github.com>
2018-09-16 02:15:54 +0200

    fixed determination of output file name length

--- examples/parameter_io/f0analysis.cpp
+++ examples/parameter_io/f0analysis.cpp
@@ -50,7 +50,7 @@ int SetOption(int argc, char **argv, double *f0_floor, double *f0_ceil,
     if (strcmp(argv[argc], "-c") == 0) *f0_ceil = atof(argv[argc + 1]);
     if (strcmp(argv[argc], "-s") == 0) *frame_period = atof(argv[argc + 1]);
     if (strcmp(argv[argc], "-o") == 0)
-      snprintf(filename, sizeof(argv[argc + 1]), argv[argc + 1]);
+      snprintf(filename, strlen(argv[argc + 1]) + 1, "%s", argv[argc + 1]);
     if (strcmp(argv[argc], "-t") == 0) *text_flag = 1;
     if (strcmp(argv[argc], "-h") == 0) {
       usage(argv[0]);
